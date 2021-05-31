9ae69c61f23d6cc2bb060bc2c809d18da1bbd597
Vladyslav Kondratenko <1999vlad1999kondratenko@gmail.com>
2021-05-31 22:30:58 +0300

    init5555

--- src/main/java/com/kondratenko/busparkemploeesworkcontrol/config/ApplicationConfig.java
+++ src/main/java/com/kondratenko/busparkemploeesworkcontrol/config/ApplicationConfig.java
@@ -17,7 +17,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("src/main/resources/messages");
+        messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
